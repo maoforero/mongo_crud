62b74ee1cce9759f685dafda8140828631f2ad23
maoforero <maforeror@gmail.com>
2021-12-31 00:45:54 -0500

    levantando servidor res.200

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+
+app.use('/', (req, res) => {
+    res.sendStatus(200);
+})
+
+app.listen(PORT, (req, res) => {
+    console.log(`It's Alive 🤖 http://localhost:${PORT}`)
+})
