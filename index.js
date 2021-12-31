676d1aa7284a5eb89e3832112ea003dea42d6de2
maoforero <maforeror@gmail.com>
2021-12-31 00:48:17 -0500

    levantando servidor res.200 comments

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require("express");
+const express = require("express"); // Import express
 const app = express();
-const PORT = 3000;
+const PORT = 3000; // Puerto de escucha
 
+//App iniciando desde cualquier ruta
 app.use('/', (req, res) => {
     res.sendStatus(200);
 })
 
+//App escuchando puerto 3000
 app.listen(PORT, (req, res) => {
     console.log(`It's Alive 🤖 http://localhost:${PORT}`)
 })
