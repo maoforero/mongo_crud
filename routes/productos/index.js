640df6495cd380f78e4b85d2adbf2f9b4b23656a
maoforero <maforeror@gmail.com>
2021-12-31 09:11:44 -0500

    peticiones basicas

--- routes/productos/index.js
+++ routes/productos/index.js
@@ -6,6 +6,19 @@ const appleRouter = require('./apple');
 const samsungRouter = require('./samsung');
 const xiaomiRouter = require('./xiaomi');
 
+
+router.post('/', (req, res) => {
+    res.send('Post productos')
+});
+
+router.delete('/', (req, res) => {
+    res.send('delete')
+});
+
+router.patch('/', (req, res) => {
+    res.send('patch')
+});
+
 router.get('/', (req, res) => {
     res.json(['Productos'])
 });
